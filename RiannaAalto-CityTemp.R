043b3f93222319843a9a0881342004e359baae59
Rianna Aalto <rianna.aalto@gmail.com>
2023-05-16 11:46:08 +0200

    Add files via upload

---
+++ RiannaAalto-CityTemp.R
@@ -0,0 +1,140 @@
+
+************************************************************************************************************
+  *** I'm so sorry Ishi, I dont have a working algorithm for this exercise and I'm how to get a multiple input from user with two types of variable and save it as a dataframe and then calculate the min max and average. Tried watching several tutorial but nothing seems to work according to how I visualized it.
+***********************************************************************************************************
+  ***********************************************************************************************************
+  
+  # taking input from the user
+  
+  # string input
+  var1 = readline(prompt = "Enter City Name : ")
+  
+  # character input
+  var2 = readline(prompt = "Enter Temperature : ")
+  # convert to integer
+  var2 = as.integer(var2)
+  
+  # printing values
+  print(var1)
+  print(var2)
+  
+  ------
+    # Manual Calculation min, max, and average function
+    x = va2
+  Minx <- function(x){
+    Min <- Inf
+    for(i in x){
+      if(i<Min){
+        Min <- i
+      }
+    }
+    Min
+  }
+  
+  #Max 
+  n <- 20:200
+  MAX = -Inf    ## initialize maximum
+  x <- numeric(length(n))    ## vector to hold record
+  for (i in 1:length(n)) {
+    x[i] <- qbinom(0.05, n[i], 0.47) - 1
+    if (x[i] > MAX) MAX <- x[i]
+  }
+  
+  ## check the first few values of `x`
+  head(x)
+  # [1] 5 5 6 6 6 7
+  
+  MAX
+  # [1] 81
+  --------
+    ct_1 <- c("Stockholm")
+  ct_2 <- c("GÃ¶teborg")
+  ct_3 <- c("Uppsala")
+  ct_4 <- c("LinkÃ¶ping")
+  ct_5 <- c("NorrkÃ¶ping")
+  ct_6 <- c("VÃ¤sterÃ¥s")
+  ct_7 <- c("UmeÃ¥")
+  ct_8 <- c("LuleÃ¥")
+  ct_9 <- c("Lund")
+  ct_10 <- c("MalmÃ¶")
+  
+  temp1 <- readline(prompt = "Enter week 1 temperature"[ct_1:ct_10], nrow=10), )
+city_temp <- data.frame(City Name = (ct_1:ct_10), Week1=temp1, Week2=temp2, Week3=temp3, Week4=temp4)
+print("Enter below the Temperature for the month of September")
+temp1 <- readline(prompt = "Enter week 1 temperature:")
+for (i in (temp1:ct_:ct_10)
+     else()
+     
+     # input month temperature
+     ct_1 <- c("Stockholm")
+     ct_2 <- c("GÃ¶teborg")
+     ct_3 <- c("Uppsala")
+     ct_4 <- c("LinkÃ¶ping")
+     ct_5 <- c("NorrkÃ¶ping")
+     ct_6 <- c("VÃ¤sterÃ¥s")
+     ct_7 <- c("UmeÃ¥")
+     ct_8 <- c("LuleÃ¥")
+     ct_9 <- c("Lund")
+     ct_10 <- c("MalmÃ¶")
+     
+     printf("Enter below the Temperature for the month of September")
+     Temp1 <- readline(prompt = "Enter" ct_1 "weekly temperature:")
+     
+     
+     
+     
+     
+     set current month
+     input 5 temperature each cities
+     functions to compute  min, max and average temp of each city
+     1) using manual calc
+2) using built-in r function
+
+column 1 City
+column 2 temp1
+column 3 temp2
+column 4 temp3
+column 5 temp4
+------
+  test
+ct_1 <- c("Stockholm")
+ct_2 <- c("GÃ¶teborg")
+ct_3 <- c("Uppsala")
+ct_4 <- c("LinkÃ¶ping")
+ct_5 <- c("NorrkÃ¶ping")
+ct_6 <- c("VÃ¤sterÃ¥s")
+ct_7 <- c("UmeÃ¥")
+ct_8 <- c("LuleÃ¥")
+ct_9 <- c("Lund")
+ct_10 <- c("MalmÃ¶")
+print("Enter below the Temperature for the month of September")
+city_temp <- data.frame(City Name = (ct_1:ct_10), Week1=temp1, Week2=temp2, Week3=temp3, Week4=temp4)
+temp1 <- readline(prompt = "Enter week 1 temperature"
+                  
+                  temp1 <- readline(prompt = "Enter week 1 temperature:")
+                  for (i in (temp1:ct_:ct_10)
+                       else()
+                       
+                       -------
+                       
+                       ct1 <- c("Stockholm")
+                       ct1_a <- as.integer (readline(prompt = "Enter 1stweek temperature in Stockholm:"))
+                       ct1_b <- as.integer (readline(prompt = "Enter 2ndweek temperature in Stockholm:"))
+                       ct1_c <- as.integer (readline(prompt = "Enter 3rdweek temperature in Stockholm:"))
+                       ct1_d <- as.integer (readline(prompt = "Enter 4thweek temperature in Stockholm:"))
+                       
+                       ### create the dataframe 
+                       my_basket = data.frame(City = c("Fruit","Fruit","Fruit","Fruit","Fruit","Vegetable","Vegetable","Vegetable","Vegetable","Dairy","Dairy","Dairy","Dairy","Dairy"), 
+                                              ITEM_NAME = c("Apple","Banana","Orange","Mango","Papaya","Carrot","Potato","Brinjal","Raddish","Milk","Curd","Cheese","Milk","Paneer"),
+                                              Price = c(100,80,80,90,65,70,60,70,25,60,40,35,50,120),
+                                              Tax = c(2,4,5,6,2,3,5,1,3,4,5,6,4,3))
+                       my_basket
+                       ----------------------------
+                         
+                         st_1 <- as.integer(readline(prompt="Stockholm weekly temperature: "))
+                       st_2 <- as.integer(readline(prompt="Stockholm weekly temperature: "))
+                       st_3 <- as.integer(readline(prompt="Stockholm weekly temperature: "))
+                       st_4 <- as.integer(readline(prompt="Stockholm weekly temperature: "))
+                       
+                       -------
+                         
